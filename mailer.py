4b4d46a4df6b10d3c17e8c2b1e19af349585ef1e
ElManchacho <pld.pro@hotmail.com>
2022-04-06 11:03:21 +0200

    loopCustomInterval

--- mailer.py
+++ mailer.py
@@ -1,5 +1,7 @@
+from datetime import date
 import smtplib, ssl
-import json
+from sqlite3 import Date
+import json, datetime
 
 json_file = open("env.json")
 variables = json.load(json_file)
@@ -13,14 +15,28 @@ email_password = variables["password"]
 
 email_receiver = variables["receiver"]
 
+def sendMail():
+  with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
+
+    server.login(email_address, email_password)
+
+    server.sendmail(email_address, email_receiver, message)
+
+dateStart = datetime.datetime.today()
+
+interval = int(input("Veuillez saisir l'intervalle de temps entre chaque envoi en minutes : \n")) # en minutes
+
 body = 'Madame, Monsieur, \n\n Je me permet de vous recontacter afin d\'obtenir des nouvelles quant a l\'envoi de nos notes sur l\'espace MyEfrei.\nEn attente de votre retour.\n\nCordialement,\n\nLEROY DUCARDONNOY Paul'
 
 message = 'Subject: {}\n\n{}'.format("Informations notes", body)
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
+stop = ""
 
-  server.login(email_address, email_password)
-
-  server.sendmail(email_address, email_receiver, message)
+while stop != "stop":
+  diffTmstp = datetime.datetime.today().timestamp() - dateStart.timestamp()
+  if (diffTmstp >= interval * 60):
+    sendMail()
+    print("Mail sent at "+ str(datetime.datetime.today()) )
+    dateStart = datetime.datetime.today()
